152e3586a66c2352add080573248fe2ab5b4f9e3
djsereno <djsereno91@gmail.com>
2023-03-17 15:19:48 -0700

    Add task factory function

---
+++ src/task.js
@@ -0,0 +1,35 @@
+const Task = (title) => {
+  let _title = title;
+  let _description = '';
+  let _dueDate = '';
+  let _priority = '';
+
+  const getTitle = () => _title;
+  const getDescription = () => _description;
+  const getDueDate = () => _dueDate;
+  const getPriority = () => _priority;
+
+  const setTitle = (newTitle) => {
+    _title = newTitle;
+  };
+  const setDescription = (newDescription) => {
+    _description = newDescription;
+  };
+  const setDueDate = (newDueDate) => {
+    _dueDate = newDueDate;
+  };
+  const setPriority = (newPriority) => {
+    _priority = newPriority;
+  };
+
+  return {
+    getTitle,
+    getDescription,
+    getDueDate,
+    getPriority,
+    setTitle,
+    setDescription,
+    setDueDate,
+    setPriority,
+  };
+};
